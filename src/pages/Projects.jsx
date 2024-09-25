94493ef207d4bb3db1d7bcc64a60b62218f94e88
Robyn Carnow <robynainsley2i@gmail.com>
2024-09-25 20:56:25 +0200

    Projects page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,48 @@
+import Loader from "../components/Loader";
+import React, { useState, useEffect } from "react";
 export default function Projects() {
-    return (
-        <div>
-            <h1>This is the projects page</h1>
-        </div>
-    );
-}   
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true); // New loading state
+
+  useEffect(() => {
+    fetch("https://robynainsley21.github.io/vue-data-eomp/data/")
+      .then((response) => response.json())
+      .then((jsonData) => {
+        setData(jsonData);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <Loader />;
+
+  if (!data || !data.about) return <Loader />;
+  return (
+    <div id="projects">
+      <h1>Some of my projects</h1>
+
+      <div>
+        {data.projects.map((project) => (
+          /* From Uiverse.io by andrew-demchenk0 */
+          <div className="card">
+            <div className="card__img">
+              <img src={project.img_url} alt="project-img" />
+            </div>
+            <div className="card__avatar">
+              <img src={project.language_img} alt="lang-img" />
+            </div>
+            <div className="card__title">{project.projectName}</div>
+            <div className="card__subtitle">{project.description}</div>
+            <div className="card__wrapper">
+              <button className="card__btn">Button</button>
+              <button className="card__btn card__btn-solid">Button</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
